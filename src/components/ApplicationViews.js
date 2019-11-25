2e65e548b4358411944257c64b3a536b86af34b5
Ryan Crowley <ryancrowleycode@gmail.com>
2019-11-25 15:45:53 -0600

    added code to NavBar.js and ApplicationViews.js

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,22 @@
+import { Route } from 'react-router-dom'
+import React, { Component } from 'react'
+import Home from './home/Home'
+import AnimalCard from './animal/AnimalCard'
+
+class ApplicationViews extends Component {
+
+    render() {
+        // setting up all of the routes, so that when the url matches the path, React will know what component to render
+        <React.Fragment>
+            <Route exact path="/" render={(props) => {
+                return <Home />
+            }} />
+            <Route path="/animals" render={(props) => {
+                return <AnimalCard />
+            }} />
+        </React.Fragment>
+    }
+
+}
+
+export default ApplicationViews
