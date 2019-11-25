8642b3510d5396249b7aae769eed362e434e11fa
Ryan Crowley <ryancrowleycode@gmail.com>
2019-11-25 14:35:58 -0600

    added directories for employee, locaiton, and owner. Added info to EmployeCard.js

---
+++ src/components/employee/EmployeeCard.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class EmployeeCard extends Component {
+    render() {
+        return (
+            <div className="card">
+                <div className="card-content">
+                    <picture>
+                        <img src={require('./dog.svg')} />
+                    </picture>
+                    <h3>Name: <span className="card-petname">David Dogperson</span></h3>
+                    <p>Info: David is awesome! 500 years experience</p>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default EmployeeCard
