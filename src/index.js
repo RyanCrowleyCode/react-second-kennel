d540acd30175312d5ddff7f5dd543ab3a5731a20
Ryan Crowley <ryancrowleycode@gmail.com>
2019-11-25 15:29:19 -0600

    added alt to images

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Kennel from './Kennel';
 import * as serviceWorker from './serviceWorker';
+import Kennel from './components/Kennel';
 
 ReactDOM.render(<Kennel />, document.getElementById('root'));
 
