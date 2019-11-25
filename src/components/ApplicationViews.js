c5feb06fde9054de751d921cd2615b439fbde030
Ryan Crowley <ryancrowleycode@gmail.com>
2019-11-25 15:56:43 -0600

    Kennel.js now renders ApplicationViews and NavBar. index.js now has Router tags wrapped around Kennel, and is imporoting Browser Router from react-router-dom

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -6,15 +6,17 @@ import AnimalCard from './animal/AnimalCard'
 class ApplicationViews extends Component {
 
     render() {
-        // setting up all of the routes, so that when the url matches the path, React will know what component to render
-        <React.Fragment>
-            <Route exact path="/" render={(props) => {
-                return <Home />
-            }} />
-            <Route path="/animals" render={(props) => {
-                return <AnimalCard />
-            }} />
-        </React.Fragment>
+        return (
+            // setting up all of the routes, so that when the url matches the path, React will know what component to render
+            <React.Fragment>
+                <Route exact path="/" render={(props) => {
+                    return <Home />
+                }} />
+                <Route path="/animals" render={(props) => {
+                    return <AnimalCard />
+                }} />
+            </React.Fragment>
+        )
     }
 
 }
